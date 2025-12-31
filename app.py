c6ec6d9e81656dcc7f22eedfdfc288d0881a1446
Bevis <add412@gmail.com>
2025-12-31 23:50:52 +0800

    <modify>：修改app.py設定

--- app.py
+++ app.py
@@ -197,4 +197,4 @@ with gr.Blocks(title="直播人力安排預測系統",
             )
 
 if __name__ == "__main__":
-    demo.launch(server_name="127.0.0.1", server_port=7860, share=False)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
